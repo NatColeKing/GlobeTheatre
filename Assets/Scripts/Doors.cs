8e1ca47e51170637ad9251bb212a15faa071d386
Nat <natalie1vm@hotmail.com>
2019-03-12 14:43:57 -0700

    Trap doors now snap open

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,61 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Doors : MonoBehaviour
+    {
+      public VRTK_InteractableObject linkedObject;
+      public GameObject door;
+      public Vector3 ogAngle;
+      public Vector3 newAngle;
+
+      protected Transform spinner;
+      protected bool spinning;
+
+      protected virtual void OnEnable()
+      {
+          // door is closed
+          // door.transform.rotation = Quaternion.Euler(ogAngle);
+          spinning = false;
+          linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+          if (linkedObject != null)
+          {
+              linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+              linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+          }
+
+          spinner = transform.Find("Capsule");
+      }
+
+      protected virtual void OnDisable()
+      {
+          if (linkedObject != null)
+          {
+              linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+              linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+          }
+      }
+
+      protected virtual void Update()
+      {
+          if (spinning)
+          {
+              // open door
+              door.transform.localRotation = Quaternion.Euler(newAngle);
+          }
+      }
+
+      protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+      {
+          spinning = true;
+      }
+
+      protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+      {
+          // close door
+          door.transform.localRotation = Quaternion.Euler(ogAngle);
+          spinning = false;
+      }
+  }
+}
---
+++ Assets/Scripts/InfoBubbles.cs
@@ -0,0 +1,56 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class InfoBubbles : MonoBehaviour
+    {
+      public VRTK_InteractableObject linkedObject;
+      public GameObject blurb;
+
+      protected Transform spinner;
+      protected bool spinning;
+
+      protected virtual void OnEnable()
+      {
+          blurb.SetActive(false);
+          spinning = false;
+          linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+          if (linkedObject != null)
+          {
+              linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+              linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+          }
+
+          spinner = transform.Find("Capsule");
+      }
+
+      protected virtual void OnDisable()
+      {
+          if (linkedObject != null)
+          {
+              linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+              linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+          }
+      }
+
+      protected virtual void Update()
+      {
+          if (spinning)
+          {
+              blurb.SetActive(true);
+          }
+      }
+
+      protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+      {
+          spinning = true;
+      }
+
+      protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+      {
+          blurb.SetActive(false);
+          spinning = false;
+      }
+  }
+}
