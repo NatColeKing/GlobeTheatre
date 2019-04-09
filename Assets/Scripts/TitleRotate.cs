08ebf696d96c9960229a09ce7fd330320210b779
Nat <natalie1vm@hotmail.com>
2019-04-09 10:50:14 -0700

    Rotations up to 'Hut' finished

--- Assets/Scripts/TitleRotate.cs
+++ Assets/Scripts/TitleRotate.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class TitleRotate : MonoBehaviour {
 
 	public Transform target;
+	public Vector3 rotation;
 
   void Update()
   {
 		// Rotate the camera every frame so it keeps looking at the target
     transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
-		transform.RotateAround(transform.position, Vector3.up, 90);
+		transform.RotateAround(transform.position, rotation, 90);
   }
 }
